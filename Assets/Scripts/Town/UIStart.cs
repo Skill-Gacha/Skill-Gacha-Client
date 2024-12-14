ddd8b918b7b7ff43c76f74a12e198ea2538e0a22
Ayumudayo <yumemiayumu@gmail.com>
2024-12-14 20:22:16 +0900

    🩹 fix: 주소 변경 및 프로젝트 이름 변경

--- Assets/Scripts/Town/UIStart.cs
+++ Assets/Scripts/Town/UIStart.cs
@@ -21,9 +21,9 @@ public class UIStart : MonoBehaviour
     private TMP_Text placeHolder;
 
     private int classIdx = 0;
-    
-    //private string serverUrl = "58.229.131.23";
-    private string serverUrl = "52.195.154.2";
+
+    private string serverUrl = "58.229.131.23";
+    //private string serverUrl = "52.195.154.2";
     private string nickname;
     private string port = "5555";
 
